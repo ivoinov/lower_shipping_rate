c343e6ed67854094980aabe36ac4b8c6529c93d2
Ilya Voinov <ilya.voinov@yahoo.com>
2018-03-07 22:14:02 +0200

    Initial commit

---
+++ app/code/local/Ivoinov/LowerShippingMethod/Helper/Data.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * *
+ *  * PHP version 5
+ *  *
+ *  * LICENSE: This source file is subject to version 3.01 of the PHP license
+ *  * that is available through the world-wide-web at the following URI:
+ *  * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ *  * the PHP License and are unable to obtain it through the web, please
+ *  * send a note to license@php.net so we can mail you a copy immediately.
+ *  *
+ *  * @category   CategoryName
+ *  * @package    PackageName
+ *  * @author     Ilya Voinov <ilya.voinov@yahoo.com>
+ *  * @copyright  1997-2018 The PHP Group
+ *  * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ *
+ */
+
+/**
+ * Class Ivoinov_LowerShippingMethod_Helper_Data
+ */
+class Ivoinov_LowerShippingMethod_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    CONST IS_LOWER_SHIPPING_METHOD_ENABLED_XML_XPATH = 'shipping/option/select_lower_shipping_method';
+
+    /**
+     * @return bool
+     */
+    public function isLowerShippingMethodSelectEnabled()
+    {
+        return Mage::getStoreConfigFlag(self::IS_LOWER_SHIPPING_METHOD_ENABLED_XML_XPATH);
+    }
+}
+
